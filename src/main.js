7479115c672f10ea01f539eb24692fc741635ece
riyazshaik2638 <riyazshaik2638@gmail.com>
2017-02-26 16:26:45 -0500

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var datepicker_module_1 = require("./datepicker.module");
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(datepicker_module_1.DatepickerModule);
+//# sourceMappingURL=main.js.map
